0332007a83841f6b35399dea742539b8127f4f4e
MARVYN PHAM <116658971+phamm270@users.noreply.github.com>
2023-01-09 20:58:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from webbot import Browser
+print("Installation complete!")
+print("Starting...")
+web = Browser()
+print("Finished!")
+print("Chromium is now ready to use!")
+print("If you do not see anything please tell me.")
